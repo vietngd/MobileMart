1dc2d00ec89bee20955e9aad69c67fb553551bfa
Nghiêm Hồng <hung24855@gmail.com>
2024-05-26 23:16:47 +0700

    update env

--- src/config/ConnectDB.js
+++ src/config/ConnectDB.js
@@ -3,11 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db_name = process.env.DB_NAME;
+const db_host = process.env.DB_HOST;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWROD;
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: db_host,
+  user: db_user,
+  password: db_password,
   database: db_name,
 });
 

f958c44bef0bd6f0c2c5c9bebd97d0d4fc357798
Nguyen Duc Viet <vietnguyen@authenticeducationhub.com>
2024-10-04 09:15:02 +0700

    fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"]
   })
 );
 
